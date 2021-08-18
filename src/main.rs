700fb5694e29a157900df60afbb3a3efaed636cc
cappww <cappw1@gmail.com>
2021-08-18 08:57:37 -0700

    minor adjustmetents

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,11 @@ fn main() {
     let client = UserAuthDefaultClient::new(auth);
 
     dbx_list_files(&client, "");
-    //dbx_write_file(&client, "");
 }   
 
 fn dbx_list_files (client: &UserAuthDefaultClient, path: &str) {
-    let folder_path = &files::ListFolderArg::new(path.to_string());
-    let result = files::list_folder(client, folder_path).unwrap().unwrap();
+    let folder_path = files::ListFolderArg::new(path.to_string());
+    let result = files::list_folder(client, &folder_path).unwrap().unwrap();
 
     for entry in result.entries {
         match entry {
